e71740a04135fb753f7944d183e29db25f7c9d7c
lethanhtuan939 <thanhtuanle939#gmail.com>
2024-09-12 15:15:20 +0700

    modify: change and responsive sidebar, header

--- src/App.vue
+++ src/App.vue
@@ -1,69 +1,68 @@
 <template>
-  <div class="home-page">
-    <Header class="header"></Header>
-    <Navbar class="navbar"></Navbar>
-    <div class="content">
-      <RouterView />
+  <div class="wrapper">
+    <div class="d-flex">
+      <Sidebar />
+      <div class="flex-grow-1">
+        <Header />
+        <div class="content p-4">
+          <RouterView />
+        </div>
+        <!-- <Footer /> -->
+      </div>
     </div>
   </div>
-  <Footer class="footer"></Footer>
 </template>
+
 <script setup>
 import Header from './components/Header/Header.vue';
-import Navbar from './components/Navbar/Navbar.vue';
-import 'vue3-toastify/dist/index.css';
-import { RouterView } from 'vue-router';
+import Sidebar from './components/Navbar/Navbar.vue';
 import Footer from './components/Footer/Footer.vue';
-import '@fortawesome/fontawesome-free/css/all.min.css';
+import { RouterView } from 'vue-router';
+import { ref, provide } from 'vue';
+
+const isSidebarCollapsed = ref(false);
+
+const toggleSidebar = () => {
+  isSidebarCollapsed.value = !isSidebarCollapsed.value;
+};
+
+provide('isSidebarCollapsed', isSidebarCollapsed);
+provide('toggleSidebar', toggleSidebar);
+
 </script>
-<style>
-:root {
-  --text-color: #ADB8CC;
-}
 
-body {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
-
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2C3E50;
-}
-
-.home-page {
-  display: grid;
-  grid-template-areas:
-    "header header header header"
-    "navbar content content content"
-    "navbar content content content"
-    ". footer footer footer";
-  grid-template-columns: 250px 1fr 1fr 1fr;
-  grid-template-rows: auto 1fr auto;
+<style scoped>
+.wrapper {
   min-height: 100vh;
+  display: flex;
+  flex-direction: row;
 }
 
-.header {
-  grid-area: header;
-}
-
-.navbar {
-  grid-area: navbar;
+.d-flex {
+  display: flex;
+  flex-grow: 1;
+  flex-direction: row;
 }
 
 .content {
-  grid-area: content;
-  margin: 10px 20px;
-  width: calc(100% - 40px);
+  flex-grow: 1;
+  background-color: #f8f9fa;
 }
 
-.footer {
-  grid-area: footer;
-  padding: 20px;
-  background-color: #f8f9fa;
-  text-align: center;
+.sidebar {
+  width: 250px;
+  min-width: 250px;
+}
+
+@media (max-width: 768px) {
+  .content {
+    margin-left: 0;
+  }
+
+  .sidebar {
+    width: 100%;
+    position: absolute;
+    z-index: 1;
+  }
 }
 </style>
