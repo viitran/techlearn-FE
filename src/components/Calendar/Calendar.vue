18dddc9e5b4167016841f3bc149c78acf51c191f
viitran <tieuvi200904@gmail.com>
2024-09-13 09:18:10 +0700

    fix bug/feature/28- fix UI show avatar teacher

--- src/components/Calendar/Calendar.vue
+++ src/components/Calendar/Calendar.vue
@@ -1,85 +1,72 @@
 <template>
-    <ejs-schedule height="750px" width="100%" ref='scheduleObj'
-      :selectedDate="selectedDate"
-      :eventSettings="eventSettings"
-      :actionBegin="onActionBegin"
-      class="calendar"
-     :editorTemplate="'editorTemplate'"
-     :eventRendered="onEventRendered"
-     :startHour="startHour"
-     :endHour="endHour"
-    >
+  <ejs-schedule height="750px" width="100%" ref='scheduleObj' :selectedDate="selectedDate"
+    :eventSettings="eventSettings" :actionBegin="onActionBegin" class="calendar" :editorTemplate="'editorTemplate'"
+    :eventRendered="onEventRendered" :startHour="startHour" :endHour="endHour">
     <template v-slot:editorTemplate>
       <table class="custom-event-editor" width="100%" cellpadding="5">
         <tbody>
-              <tr>
-                <td class="e-textlabel">Title</td>
-                <td colspan="4">
-                  <input id="Subject" class="e-field e-input" type="text" value="" name="Subject" style="width: 100%" />
-                </td>
-              </tr>
-              <tr>
-                <td class="e-textlabel">Status</td>
-                <td colspan="4"><ejsDropdownlist
-                  id='OwnerId'
-                  name="OwnerId"
-                  class="e-field"
-                  placeholder= 'Choose status'
-                  :dataSource='ownerDataSource'
-                  :fields="dropListFields" >
-                    </ejsDropdownlist>
-                </td>
-              </tr>
-              <tr>
-                <td class="e-textlabel">From</td>
-                <td colspan="4"><ejsDatetimepicker id="StartTime" class="e-field" name="StartTime"></ejsDatetimepicker>
-                </td>
-              </tr>
-              <tr>
-                <td class="e-textlabel">To</td>
-                <td colspan="4"><ejsDatetimepicker id="EndTime" class="e-field" name="EndTime" ></ejsDatetimepicker>
-                </td>
-              </tr>
-              <tr>
-                <td class="e-textlabel">Description</td>
-                <td colspan="4">
-                  <textarea id="Description" class="e-field e-input" name="Description" rows="3" cols="50"
-                    style="width: 100%; height: 60px !important; resize: vertical"></textarea>
-                </td>
-              </tr>
-            </tbody>
+          <tr>
+            <td class="e-textlabel">Tiêu đề</td>
+            <td colspan="4">
+              <input id="Subject" class="e-field e-input" type="text" value="" name="Subject" style="width: 100%" />
+            </td>
+          </tr>
+          <tr>
+            <td class="e-textlabel">Giảng viên</td>
+            <td colspan="4">
+              <ejsDropdownlist id='OwnerId' name="OwnerId" class="e-field" placeholder='Choose status'
+                :dataSource='ownerDataSource' :fields="dropListFields">
+              </ejsDropdownlist>
+            </td>
+          </tr>
+          <tr>
+            <td class="e-textlabel">Giờ bắt đầu</td>
+            <td colspan="4">
+              <ejsDatetimepicker id="StartTime" class="e-field" name="StartTime"></ejsDatetimepicker>
+            </td>
+          </tr>
+          <tr>
+            <td class="e-textlabel">Giờ kết thúc</td>
+            <td colspan="4">
+              <ejsDatetimepicker id="EndTime" class="e-field" name="EndTime"></ejsDatetimepicker>
+            </td>
+          </tr>
+          <tr>
+            <td class="e-textlabel">Mô tả</td>
+            <td colspan="4">
+              <textarea id="Description" class="e-field e-input" name="Description" rows="3" cols="50"
+                style="width: 100%; height: 60px !important; resize: vertical"></textarea>
+            </td>
+          </tr>
+        </tbody>
       </table>
     </template>
-      <e-views>
-        <e-view option="Day"></e-view>
-        <e-view option="Week"></e-view>
-        <e-view option="WorkWeek"></e-view>
-        <e-view option="Month"></e-view>
-        <e-view option="Agenda"></e-view>
-      </e-views>
-      <e-resources>
-        <e-resource
-          field="OwnerId"
-          title="Owner"
-          name="Owners"
-          :dataSource="ownerDataSource"
-          textField="OwnerText"
-          idField="Id"
-          colorField="OwnerColor"
-        >
-        </e-resource>
-      </e-resources>
-    </ejs-schedule>
+    <e-views>
+      <e-view option="Day"></e-view>
+      <e-view option="Week"></e-view>
+      <e-view option="WorkWeek"></e-view>
+      <e-view option="Month"></e-view>
+      <e-view option="Agenda"></e-view>
+    </e-views>
+    <e-resources>
+      <e-resource field="OwnerId" title="Owner" name="Owners" :dataSource="ownerDataSource" textField="OwnerText"
+        idField="Id" colorField="OwnerColor">
+      </e-resource>
+    </e-resources>
+  </ejs-schedule>
 </template>
 
 <script setup>
-import { onMounted, provide, ref,nextTick } from "vue";
-import {ScheduleComponent as EjsSchedule, ViewsDirective as EViews, ViewDirective as EView, ResourcesDirective as EResources, ResourceDirective as EResource,
-  Day, Week, WorkWeek, Month, Agenda, DragAndDrop} from "@syncfusion/ej2-vue-schedule";
-import { DataManager ,WebApiAdaptor} from "@syncfusion/ej2-data";
+import { onMounted, provide, ref, nextTick } from "vue";
+import {
+  ScheduleComponent as EjsSchedule, ViewsDirective as EViews, ViewDirective as EView, ResourcesDirective as EResources, ResourceDirective as EResource,
+  Day, Week, WorkWeek, Month, Agenda, DragAndDrop
+} from "@syncfusion/ej2-vue-schedule";
+import { DataManager, WebApiAdaptor } from "@syncfusion/ej2-data";
 import axios from "axios";
-import { DropDownListComponent as ejsDropdownlist} from "@syncfusion/ej2-vue-dropdowns";
-import { DateTimePickerComponent  as ejsDatetimepicker} from "@syncfusion/ej2-vue-calendars";
+import { DropDownListComponent as ejsDropdownlist } from "@syncfusion/ej2-vue-dropdowns";
+import { DateTimePickerComponent as ejsDatetimepicker } from "@syncfusion/ej2-vue-calendars";
+import { toast } from 'vue3-toastify';
 import { watch } from "vue";
 const rootApi = process.env.VUE_APP_ROOT_API;
 const props = defineProps(['url', 'id']);
@@ -105,16 +92,16 @@ const eventSettings = ref({
 const startHour = "08:00";
 const endHour = "20:00";
 
-const dropListFields= {
-  text:"OwnerText",
-  value:"Id"
+const dropListFields = {
+  text: "OwnerText",
+  value: "Id"
 }
 
 const getOwnerDataSource = async () => {
   const res = await axios.get(`${props.url}/teachers/`);
   ownerDataSource.value = res.data;
-
 }
+
 const getEvent = async () => {
   try {
     const res = await axios.get(`${rootApi}/teacher-calendar/find-by-id/${props.id}`);
@@ -132,16 +119,19 @@ const getEvent = async () => {
 ;
 
 const onEventRendered = (args) => {
-  const ownerIds = args.data.OwnerIds || [];
-  const avatarsHtml = ownerIds.map(ownerId => {
+  const ownerId = args.data.OwnerId;
+  if (ownerId) {
     const owner = ownerDataSource.value.find(owner => owner.Id === ownerId);
-    return owner ? `<div class="mx-1"><img width="30" src="${owner.avatar}" class="owner-avatar rounded-circle img-fluid border border-white" /></div>` : '';
-  }).join('');
-
-  const isSingleAvatar = ownerIds.length === 1;
-  args.element.innerHTML = !isSingleAvatar
-    ? `<div class="d-flex justify-content-center mt-1">${avatarsHtml}</div><div class="d-flex justify-content-center">${args.data.Subject}</div>`
-    : `<div class="d-flex align-items-center mt-1">${avatarsHtml}<div class="">${args.data.Subject}</div></div>`;
+    if (owner) {
+      const avatarHtml = `<div class="mx-1">
+                            <img width="30" src="${owner.avatar}" 
+                              class="owner-avatar rounded-circle img-fluid border border-white" />
+                          </div>`;
+      args.element.innerHTML = `<div class="d-flex align-items-center mt-1">
+                                  ${avatarHtml}<div class="">${args.data.Subject}</div>
+                                </div>`;
+    }
+  }
 };
 
 const formatDate = (dateStr) => {
@@ -162,18 +152,24 @@ const onActionBegin = async (args) => {
         StartTime: formatDate(eventData.StartTime),
         EndTime: formatDate(eventData.EndTime),
       };
-      console.log(formattedEventData);
-
       await axios.post(`${props.url}/teacher-calendar`, formattedEventData);
-      
+      toast.success('Thêm lịch thành công!', {
+        autoClose: 2000
+      });
     } catch (error) {
       console.error('Error adding event:', error);
+      toast.error('Thêm lịch thất bại!Bạn phải thêm lịch vào ngày và giờ lớn hơn ngày và giờ hiện tại');
     }
   } else if (args.requestType === 'eventRemove') {
     try {
       await axios.delete(`${props.url}/teacher-calendar/${args.data[0].Id}`);
+      toast.success('Bạn đã Xóa lịch thành công!', {
+        autoClose: 2000
+      });
     } catch (error) {
       console.error('Error deleting event:', error);
+      toast.error('Không thể xóa sự kiện!');
+
     }
   } else if (args.requestType === 'eventChange') {
     try {
@@ -187,16 +183,21 @@ const onActionBegin = async (args) => {
       console.log(formattedEventData);
 
       await axios.put(`${props.url}/teacher-calendar/${formattedEventData.Id}`, formattedEventData);
+      toast.success('Cập nhật lại lịch thành công!', {
+        autoClose: 2000
+      });
     } catch (error) {
       console.error('Error updating event:', error);
+      toast.error('Cập nhật lịch thất bại!');
+
     }
   }
   await nextTick(() => {
     scheduleObj.value.refreshEvents();
   });
-  }
+}
 
-onMounted(() =>{
+onMounted(() => {
   getOwnerDataSource()
 })
 
