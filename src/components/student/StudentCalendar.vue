ec64a1ce1ea5177db2597ea61f7742aa74a315d2
lethanhtuan939 <thanhtuanle939#gmail.com>
2024-09-19 08:20:33 +0700

    change student calendar

--- src/components/student/StudentCalendar.vue
+++ src/components/student/StudentCalendar.vue
@@ -2,7 +2,10 @@
     <div class="row d-md-flex justify-content-between nav-student">
         <div class="col col-md-3 d-md-flex justify-content-start align-items-center">
             <div class="img-avatar">
-                <img class="rounded-circle" :src="user.avatar" alt="my-avatar">
+                <<<<<<< HEAD <img class="rounded-circle" :src="user.avatar" alt="my-avatar">
+                    =======
+                    <img class="rounded-circle" src="https://i.pinimg.com/564x/0b/2b/52/0b2b527a5d4ad76e7ee6115e895afac2.jpg" alt="my-avatar">
+                    >>>>>>> 5f37f24 (change student calendar)
             </div>
             <div class="ms-2 d-flex flex-column align-items-center m-0">
                 <div class="font-weight-bold name-color">{{ user.name }}</div>
@@ -35,8 +38,7 @@
                             <span class="error-message">{{ chapterError }}</span>
                         </div>
                         <div>
-                            <select class="modify-select" name="chapter" v-model="chapter"
-                                @change="onChuongChange">
+                            <select class="modify-select" name="chapter" v-model="chapter" @change="onChuongChange">
                                 <option value="" disabled selected hidden>Chọn chương</option>
                                 <option class="modify-option" value="Chapter 1">Chương 1</option>
                                 <option class="modify-option" value="Chương 2">Chương 2</option>
@@ -52,8 +54,7 @@
                         <div>
                             <select class="modify-select" name="teacher" v-model="teacher">
                                 <option :value="null" disabled selected hidden>Chọn giảng viên</option>
-                                <option class="modify-option" v-for="teacher in allTeachers" :key="teacher.Id"
-                                    :value="teacher">
+                                <option class="modify-option" v-for="teacher in allTeachers" :key="teacher.Id" :value="teacher">
                                     {{ teacher.OwnerText }}
                                 </option>
                             </select>
@@ -61,10 +62,9 @@
                     </div>
                 </div>
                 <div class="col col-md-1 d-flex justify-content-center align-items-center mt-4">
-                    <button type="submit" class="btn btn-primary border-0 modify-button"
-                        style="background-color: rgb(49, 210, 242) ">
-                        <span><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
-                                class="bi bi-funnel" viewBox="0 0 16 16">
+                    <button type="submit" class="btn btn-primary border-0 modify-button" style="background-color: rgb(49, 210, 242) ">
+                        <span><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-funnel"
+                                viewBox="0 0 16 16">
                                 <path
                                     d="M1.5 1.5A.5.5 0 0 1 2 1h12a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.128.334L10 8.692V13.5a.5.5 0 0 1-.342.474l-3 1A.5.5 0 0 1 6 14.5V8.692L1.628 3.834A.5.5 0 0 1 1.5 3.5zm1 .5v1.308l4.372 4.858A.5.5 0 0 1 7 8.5v5.306l2-.666V8.5a.5.5 0 0 1 .128-.334L13.5 3.308V2z" />
                             </svg> Lọc</span>
@@ -72,17 +72,20 @@
                 </div>
             </form>
         </div>
-        <div v-if="stateButtonFormStudent === false"
-            class="col col-md-1 d-flex align-items-center justify-content-center">
-            <button @click="changeOfStateButtonStudent" class="btn btn-primary border-0 modify-button"
-                style="background-color: rgb(49, 210, 242) ">
+        <div v-if="stateButtonFormStudent === false" class="col col-md-1 d-flex align-items-center justify-content-center">
+            <button @click="changeOfStateButtonStudent" class="btn btn-primary border-0 modify-button" style="background-color: rgb(49, 210, 242) ">
                 <span><i class="fas fa-plus"></i> Tạo lịch</span>
             </button>
         </div>
     </div>
-    <div v-if="stateButtonFormStudent === true" class="row">
+    <div class="row">
         <div class="col" style="margin-top: 60px;">
-            <Calendar :url="url" :id="idGV" />
+            <Calendar v-if="stateButtonFormStudent === true" :url="url" :id="idGV" />
+
+            <div v-else>
+                <h4 class="mb-2">Danh sách lịch đã đặt</h4>
+                <Calendar :url="studentUrl" :id="idGV" />
+            </div>
         </div>
     </div>
 </template>
@@ -131,9 +134,11 @@ const user = ref({
     avatar: '',
     email: ''
 });
+const studentUrl = ref(`${rootApi}/students?userId=123e4567-e89b-12d3-a456-426614174000`);
 
-const getAllCalendars = () => {
-    url.value = `${rootApi}`
+const getAllCalendars = (newUrl) => {
+    // url.value = `${rootApi}`
+    url.value = newUrl;
 }
 
 const getAllTeacher = async () => {
@@ -176,6 +181,12 @@ const searchCalendar = handleSubmit(async (formData) => {
             idGV.value = res.data.result;
             getAllTeacher();
         }
+
+        // console.log(teacherName, technicalTeacherName, chapterName);
+
+        // const filterUrl = `${rootApi}/teacher-calendar/find-calendars?teacherName=${teacherName}&technicalTeacherName=${technicalTeacherName}&chapterName=${chapterName}`;
+
+        // getAllCalendars(filterUrl);
     } catch (error) {
         toast.error("Không có khung giờ giảng viên trong ngày hôm nay!");
     }
